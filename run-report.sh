4a68b1b257cb4bc2f33942a9507149c7c75b057f
varshaprasad96 <varshaprasad96@gmail.com>
2021-02-17 14:41:08 -0800

    Get 4.7 indexes

--- run-report.sh
+++ run-report.sh
@@ -4,11 +4,11 @@ echo The following data will be collected and report can be found at reports/ fo
 echo operator,created,company,sdkversion,operatortype, csvName
 
 go run indexdump.go \
-"db/index.db.4.6.prod:prod:4.6" \
-"db/index.db.4.6.redhat-operators:redhat:4.6" \
-"db/index.db.4.6.community-operators:community:4.6" \
-"db/index.db.4.6.redhat-marketplace-operators:marketplace:4.6" \
-"db/index.db.4.6.certified-operators:certified:4.6" \
-"db/index.db.operatorhub.io:operatorhub:4.6"
+"db/index.db.4.7.prod:prod:4.7" \
+"db/index.db.4.7.redhat-operators:redhat:4.7" \
+"db/index.db.4.7.community-operators:community:4.7" \
+"db/index.db.4.7.redhat-marketplace-operators:marketplace:4.7" \
+"db/index.db.4.7.certified-operators:certified:4.7" \
+"db/index.db.operatorhub.io:operatorhub:4.7"
 
 exit
