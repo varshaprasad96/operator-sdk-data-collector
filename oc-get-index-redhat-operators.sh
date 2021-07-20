f3f1a0f9fe68f27ce5c2af0054d7fabaec353d34
varshaprasad96 <varshaprasad96@gmail.com>
2021-07-20 09:38:05 -0700

    Jul 20 - 4.8

--- oc-get-index-redhat-operators.sh
+++ oc-get-index-redhat-operators.sh
@@ -4,15 +4,15 @@ set -x
 echo "Creating folder to save DBs if it does not exist"
 mkdir -p db/
 
-oc image extract registry.redhat.io/redhat/redhat-operator-index:v4.7 --file=/database/index.db --filter-by-os='linux/amd64'
-mv index.db db/index.db.4.7.redhat-operators
-oc image extract registry.redhat.io/redhat/certified-operator-index:v4.7 --file=/database/index.db --filter-by-os='linux/amd64'
-mv index.db db/index.db.4.7.certified-operators
-oc image extract registry.redhat.io/redhat/community-operator-index:v4.7 --file=/database/index.db --filter-by-os='linux/amd64'
-mv index.db db/index.db.4.7.community-operators
-oc image extract registry.redhat.io/redhat/redhat-marketplace-index:v4.7 --file=/database/index.db --filter-by-os='linux/amd64'
-mv index.db db/index.db.4.7.redhat-marketplace-operators
+oc image extract registry.redhat.io/redhat/redhat-operator-index:v4.8 --file=/database/index.db --filter-by-os='linux/amd64'
+mv index.db db/index.db.4.8.redhat-operators
+oc image extract registry.redhat.io/redhat/certified-operator-index:v4.8 --file=/database/index.db --filter-by-os='linux/amd64'
+mv index.db db/index.db.4.8.certified-operators
+oc image extract registry.redhat.io/redhat/community-operator-index:v4.8 --file=/database/index.db --filter-by-os='linux/amd64'
+mv index.db db/index.db.4.8.community-operators
+oc image extract registry.redhat.io/redhat/redhat-marketplace-index:v4.8 --file=/database/index.db --filter-by-os='linux/amd64'
+mv index.db db/index.db.4.8.redhat-marketplace-operators
 oc image extract quay.io/operatorhubio/catalog:latest --file=/database/index.db
 mv index.db db/index.db.operatorhub.io
-oc image extract registry-proxy.engineering.redhat.com/rh-osbs/iib-pub:v4.7 --file=/database/index.db --filter-by-os='linux/amd64'
-mv index.db db/index.db.4.7.prod
+oc image extract registry-proxy.engineering.redhat.com/rh-osbs/iib-pub:v4.8 --file=/database/index.db --filter-by-os='linux/amd64'
+mv index.db db/index.db.4.8.prod
